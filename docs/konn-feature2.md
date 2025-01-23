d69525f957f010d98338dce6087bb776058c0276
Kostadin Kostadinov <kosta@topvine.co>
2025-01-23 20:08:43 +0200

    +

---
+++ docs/konn-feature2.md
@@ -0,0 +1,6 @@
+---
+id: konn-feature2
+title: Konn Feature 2
+---
+
+Details about Konn Feature 2.
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'Place holder?',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -76,7 +76,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Welcome to Konn :)',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
